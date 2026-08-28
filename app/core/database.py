0c0f3c4dc225f3ee280bbf3cea7dcf4a9d1d8dc9
josegarcia-eng <jose.garcia@autonoma.edu.co>
2026-08-28 18:58:33 -0500

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
-
+print(f"CONECTANDO A: {settings.DATABASE_URL}", file=sys.stderr, flush=True)
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
